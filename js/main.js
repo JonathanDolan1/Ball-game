1a59d027b1fb1ffa420344c31b78e0198f17bb01
Jonathan Dolan <yonidolan1@gmail.com>
2024-05-29 18:48:43 +0300

    Limit ball size

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 function onBallClick(elBall) {
     elBall.innerText = +elBall.innerText +50
+    if (elBall.innerText>400) elBall.innerText = 100
     elBall.style.height = elBall.innerText + 'px'
     elBall.style.width = elBall.innerText + 'px'
 }
