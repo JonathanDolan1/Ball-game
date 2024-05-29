e6b8ac2b202429da45a140772bbb73dbaa683da3
Jonathan Dolan <yonidolan1@gmail.com>
2024-05-29 22:33:15 +0300

    Update onBallClick with getRandomInt

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict'
 
 function onBallClick(elBall) {
-    elBall.innerText = +elBall.innerText +50
+    elBall.innerText = +elBall.innerText + getRandomInt(20,61)
     if (elBall.innerText>400) elBall.innerText = 100
     elBall.style.height = elBall.innerText + 'px'
     elBall.style.width = elBall.innerText + 'px'
