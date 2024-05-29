da99e050ec5325cae056a53123cb28853244080e
Jonathan Dolan <yonidolan1@gmail.com>
2024-05-30 00:29:01 +0300

    Add onBall4Click func

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 'use strict'
 
 function onBallClick(elBall, maxDiameter) {
-    elBall.innerText = +elBall.innerText + getRandomInt(20,61)
-    if (elBall.innerText>maxDiameter) elBall.innerText = 100
+    elBall.innerText = +elBall.innerText + getRandomInt(20, 61)
+    if (elBall.innerText > maxDiameter) elBall.innerText = 100
     elBall.style.height = elBall.innerText + 'px'
     elBall.style.width = elBall.innerText + 'px'
     elBall.style.backgroundColor = getRandomColor()
 }
 
-function onBall3Click(){
+function onBall3Click() {
     const elBall1 = document.querySelector('.ball1')
     const elBall2 = document.querySelector('.ball2')
     const color1 = elBall1.style.backgroundColor
@@ -23,4 +23,16 @@ function onBall3Click(){
     elBall2.style.height = size1 + 'px'
     elBall2.style.width = size1 + 'px'
     elBall2.style.backgroundColor = color1
+}
+
+function onBall4Click() {
+    const elBall1 = document.querySelector('.ball1')
+    const elBall2 = document.querySelector('.ball2')
+    const elBalls = [elBall1, elBall2]
+    elBalls.forEach(elBall => {
+        elBall.innerText = +elBall.innerText - getRandomInt(20, 61)
+        if (elBall.innerText < 100) elBall.innerText = 100
+        elBall.style.height = elBall.innerText + 'px'
+        elBall.style.width = elBall.innerText + 'px'
+    })
 }
