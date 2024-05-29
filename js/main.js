2d493e727b57681ce62850dc25c2056c55972ff5
Jonathan Dolan <yonidolan1@gmail.com>
2024-05-30 01:54:21 +0300

    Create undo and redo init

--- js/main.js
+++ js/main.js
@@ -4,7 +4,19 @@ var gTimeoutId = null
 var gIntervalId = null
 var gTimeoutId2 = null
 
+var gGameHistory = []
+var gCurrGameStateIdx = 0
+
+var gGame = {
+    ball1Color: 'yellow',
+    ball2Color: 'aquamarine',
+    ball1Size: 100,
+    ball2Size: 100,
+    backgroundColor: 'black',
+}
+
 function onBallClick(elBall, maxDiameter) {
+    storeMoveInHistory()
     elBall.innerText = +elBall.innerText + getRandomInt(20, 61)
     if (+elBall.innerText > maxDiameter) elBall.innerText = 100
     elBall.style.height = elBall.innerText + 'px'
@@ -13,6 +25,7 @@ function onBallClick(elBall, maxDiameter) {
 }
 
 function onBall3Click() {
+    storeMoveInHistory()
     const elBall1 = document.querySelector('.ball1')
     const elBall2 = document.querySelector('.ball2')
     const color1 = elBall1.style.backgroundColor
@@ -30,6 +43,7 @@ function onBall3Click() {
 }
 
 function onBall4Click() {
+    storeMoveInHistory()
     const elBall1 = document.querySelector('.ball1')
     const elBall2 = document.querySelector('.ball2')
     const elBalls = [elBall1, elBall2]
@@ -42,10 +56,12 @@ function onBall4Click() {
 }
 
 function onBall5Click() {
+    storeMoveInHistory()
     document.querySelector('body').style.backgroundColor = getRandomColor()
 }
 
 function onBall6Click() {
+    storeMoveInHistory()
     document.querySelector('body').style.backgroundColor = 'black'
     const elBall1 = document.querySelector('.ball1')
     const elBall2 = document.querySelector('.ball2')
@@ -66,6 +82,7 @@ function onBall6Hover() {
 }
 
 function onBall6Leave() {
+    storeMoveInHistory()
     clearTimeout(gTimeoutId)
     clearTimeout(gTimeoutId2)
     clearGIntervalId()
@@ -76,6 +93,7 @@ function clearGIntervalId(){
 }
 
 function runFirst4BallsClickInterval() {
+    storeMoveInHistory()
     const elBall1 = document.querySelector('.ball1')
     const elBall2 = document.querySelector('.ball2')
     gIntervalId = setInterval(() => {
@@ -84,4 +102,31 @@ function runFirst4BallsClickInterval() {
         onBall3Click()
         onBall4Click()
     }, 2000)
+}
+
+function storeMoveInHistory(){
+    gGameHistory.push(copyGameState())
+}
+
+function copyGameState(){
+    gCurrGameStateIdx++
+    const gameCopy = {}
+    for (var key in gGame){
+        gameCopy[key] = gGame[key]
+    }
+    return gameCopy
+}
+
+function loadGameFromHistory(idx){
+    for (var key in gGame){
+        gGame[key] = gGameHistory[idx][key]
+    }
+}
+
+function onUndo(){
+    loadGameFromHistory(gCurrGameStateIdx--)
+}
+
+function onRedo(){
+    loadGameFromHistory(gCurrGameStateIdx++)
 }
