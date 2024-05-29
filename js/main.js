cd27da150660f2c51be857300ed8608ea220dae1
Jonathan Dolan <yonidolan1@gmail.com>
2024-05-29 22:35:09 +0300

    Add random color feature

--- js/main.js
+++ js/main.js
@@ -5,4 +5,5 @@ function onBallClick(elBall) {
     if (elBall.innerText>400) elBall.innerText = 100
     elBall.style.height = elBall.innerText + 'px'
     elBall.style.width = elBall.innerText + 'px'
+    elBall.style.backgroundColor = getRandomColor()
 }
